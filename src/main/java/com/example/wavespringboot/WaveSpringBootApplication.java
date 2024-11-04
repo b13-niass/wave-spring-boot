b926bffb8485834c901c5e7526a3600bd03a6e28
cheikh ibrahima dieng <barhamadieng66@gmail.com>
2024-11-04 11:45:49 +0000

    first commit

---
+++ src/main/java/com/example/wavespringboot/WaveSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.wavespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/wavespringboot/WaveSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wavespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
